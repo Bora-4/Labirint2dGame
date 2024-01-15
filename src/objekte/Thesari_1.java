03998f8fff5637f203e0a11d56ae3be42b49cf0b
Bora-4 <bora.rrena@fti.edu.al>
2024-01-16 00:29:10 +0100

    rregullimi i disa bugs

--- src/objekte/Thesari_1.java
+++ src/objekte/Thesari_1.java
@@ -7,9 +7,9 @@ public class Thesari_1 extends Thesaret{
 
     public Thesari_1(){
 
-        name ="Thesari1";
+        name ="Thesari";
         try{
-            image = ImageIO.read(getClass().getResourceAsStream("/thesare/thesar_1.png"));
+            image = ImageIO.read(getClass().getResourceAsStream("/thesare/thesari.png"));
 
         }catch(IOException e){
             e.printStackTrace();
